99ab140a8f64ecf6e250171b9d1bafbe96ee5821
Zoe Stokes <sekotszs@gmail.com>
2020-07-15 16:52:12 -0700

    installed helmet

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const server = require('./server')
+const server = require("./server");
 
 const PORT = 5000;
 server.listen(PORT, () => {
-    console.log("Running on localhost:", PORT)
-})
+  console.log("Running on localhost:", PORT);
+});
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const helmet = require('helmet')
 const userRouter = require("./users/userRouter");
 
 const server = express();
 
 server.use(express.json());
 server.use("/api/users", userRouter);
+server.use(helmet());
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
