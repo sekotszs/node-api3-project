72fff22b6ea2aa8df32a0d5c8e64091e3560d4fd
Zoe Stokes <sekotszs@gmail.com>
2020-07-15 17:06:01 -0700

    logger added

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require("express");
-const helmet = require('helmet')
+const helmet = require("helmet");
 const userRouter = require("./users/userRouter");
-
+const morgan = require("morgan");
 const server = express();
 
 server.use(express.json());
 server.use("/api/users", userRouter);
 server.use(helmet());
+// server.use(morgan("dev"));
+server.use(logger)
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
-
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} request`)
+  console.log(req.url);
+  console.log(Date.now())
+ 
+  next();
+}
 
 module.exports = server;
